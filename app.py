dc6cf92669ffa47b28ae32f0960849cd2a8e187f
Sam Dobson <1309834+samdobson@users.noreply.github.com>
2023-02-25 13:26:48 +0000

    Rework layout.

--- app.py
+++ app.py
@@ -8,37 +8,78 @@ import numpy as np
 import altair as alt
 import cufflinks as cf
 
-st.set_page_config(page_title='Streamlit Sandbox', page_icon=':memo:', layout='wide', initial_sidebar_state='collapsed')
+
+st.set_page_config(
+    page_title="Streamlit Sandbox",
+    page_icon=":memo:",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
 st.sidebar.title(":memo: Editor settings")
 
+st.title("Streamlit sandbox")
+st.write("Play with Streamlit live in the browser!")
+
 THEMES = [
-    "ambiance", "chaos", "chrome", "clouds", "clouds_midnight", "cobalt", "crimson_editor", "dawn",
-    "dracula", "dreamweaver", "eclipse", "github", "gob", "gruvbox", "idle_fingers", "iplastic",
-    "katzenmilch", "kr_theme", "kuroir", "merbivore", "merbivore_soft", "mono_industrial", "monokai",
-    "nord_dark", "pastel_on_dark", "solarized_dark", "solarized_light", "sqlserver", "terminal",
-    "textmate", "tomorrow", "tomorrow_night", "tomorrow_night_blue", "tomorrow_night_bright",
-    "tomorrow_night_eighties", "twilight", "vibrant_ink", "xcode"
+    "ambiance",
+    "chaos",
+    "chrome",
+    "clouds",
+    "clouds_midnight",
+    "cobalt",
+    "crimson_editor",
+    "dawn",
+    "dracula",
+    "dreamweaver",
+    "eclipse",
+    "github",
+    "gob",
+    "gruvbox",
+    "idle_fingers",
+    "iplastic",
+    "katzenmilch",
+    "kr_theme",
+    "kuroir",
+    "merbivore",
+    "merbivore_soft",
+    "mono_industrial",
+    "monokai",
+    "nord_dark",
+    "pastel_on_dark",
+    "solarized_dark",
+    "solarized_light",
+    "sqlserver",
+    "terminal",
+    "textmate",
+    "tomorrow",
+    "tomorrow_night",
+    "tomorrow_night_blue",
+    "tomorrow_night_bright",
+    "tomorrow_night_eighties",
+    "twilight",
+    "vibrant_ink",
+    "xcode",
 ]
 
 KEYBINDINGS = ["emacs", "sublime", "vim", "vscode"]
 
-display, editor = st.columns((2, 1))
+editor, app = st.tabs(["Editor", "App"])
 
-INITIAL_CODE = """st.header("Streamlit Sandbox")
-st.write("Play with Streamlit live in the browser!")
-
+INITIAL_CODE = """
 table_data = {'Column 1': [1, 2], 'Column 2': [3, 4]}
 st.write(pd.DataFrame(data=table_data))
 """
 
 with editor:
-    st.write('### Code editor')
+    st.write("### Code editor")
     code = st_ace(
         value=INITIAL_CODE,
         language="python",
         placeholder="st.header('Hello world!')",
         theme=st.sidebar.selectbox("Theme", options=THEMES, index=26),
-        keybinding=st.sidebar.selectbox("Keybinding mode", options=KEYBINDINGS, index=3),
+        keybinding=st.sidebar.selectbox(
+            "Keybinding mode", options=KEYBINDINGS, index=3
+        ),
         font_size=st.sidebar.slider("Font size", 5, 24, 14),
         tab_size=st.sidebar.slider("Tab size", 1, 8, 4),
         wrap=st.sidebar.checkbox("Wrap lines", value=False),
@@ -46,18 +87,19 @@ with editor:
         show_print_margin=True,
         auto_update=False,
         readonly=False,
-        key="ace-editor"
+        key="ace-editor",
     )
-    st.write('Hit `CTRL+ENTER` to refresh')
-    st.write('*Remember to save your code separately!*')
+    st.write("Hit `CTRL+ENTER` to refresh")
+    st.write("*Remember to save your code separately!*")
 
-with display:
+with app:
     exec(code)
 
 with st.sidebar:
-    libraries_available = st.expander('Available Libraries')
+    libraries_available = st.expander("Available Libraries")
     with libraries_available:
-        st.write("""
+        st.write(
+            """
         * Pandas (pd)
         * Numpy (np)
         * Altair (alt)
@@ -66,4 +108,5 @@ with st.sidebar:
         * Cufflinks (cf)
 
         [Need something else?](https://github.com/samdobson/streamlit-sandbox/issues/new)
-        """)
+        """
+        )
